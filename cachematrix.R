68c2c3fbd13985c39587254839e0713626d9cba7
Kornwhalice <C16john.kornahrens@usafa.edu>
2015-01-25 16:39:26 -0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,7 @@
-## Put comments here that give an overall description of what your
+##Documentation: googled problem to help with understanding and syntax
+##Put comments here that give an overall description of what your
 ## functions do
-
+##
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
